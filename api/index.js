befcc747c6a22e6b1b036e4a53d0195ac0cda589
11061994 <tazeenkhan1106@gmail.com>
2021-12-03 23:41:03 +0500

    adding dependency

--- api/index.js
+++ api/index.js
@@ -3,8 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
-const userRoute = require("./routes/user")
 dotenv.config();
 
 mongoose
@@ -20,8 +21,8 @@ mongoose
 //middleware
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running");
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,17 +1,25 @@
-const User = require("../models/User");
 const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
-//Regu=siter
-router.post("/register request", (req, res) => {
+//REGISTER
+router.post("/register", async(req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
-        img: req.body.img,
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString(),
     });
 
-    const savedUser = await newUser.save();
-    console.log(savedUser);
-})
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-module.exports = router();
+module.exports = router;
